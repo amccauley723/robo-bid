88352818e4f856276442ac64a5ffb89e133f086b
Drew <amccauley723@gmail.com>
2017-01-14 09:37:38 -0600

    add absolute path to sqlite db and start articles view

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,3 +4,5 @@ Route::get('/', 'PagesController@homePage');
 Route::get('/account-login', 'PagesController@accountLogin');
 Route::get('/view-packages', 'PagesController@viewPackages');
 Route::get('/contact', 'PagesController@contact');
+
+Route::get('articles', 'ArticlesController@index');
